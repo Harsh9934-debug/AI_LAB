6ac8c8e1d81040c6254d91465e7141f30292e0ea
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-14 17:26:38 +0530

    Updated the program

--- Eight.py
+++ Eight.py
@@ -1,33 +1,23 @@
-#write a program to solve the rat in the maze program 
+maze = [[1,0,0,0],[1,1,0,1],[0,1,0,0],[1,1,1,1]]
 
-def is_safe(maze,x,y):
-    if x>=0 and x<len(maze) and y>=0 and y<len(maze) and maze[x][y]==1:
-        return True
-    return False
-def solve_maze(maze):
-    sol=[[0 for j in range(len(maze))]for i in range(len(maze))]
-    if solve_util(maze,0,0,sol)==False:
-        print("Solution doesn't exist")
-        return False
-    print_solution(sol)
-    return True
-def solve_util(maze,x,y,sol):   
-    if x==len(maze)-1 and y==len(maze)-1:
+def solve(m):
+    n = len(m)
+    sol = [[0]*n for _ in range(n)]
+
+    def go(x,y):
+        if x<0 or y<0 or x>=n or y>=n or m[x][y]==0 or sol[x][y]:
+            return False
         sol[x][y]=1
-        return True
-    if is_safe(maze,x,y)==True:
-        sol[x][y]=1
-        if solve_util(maze,x+1,y,sol)==True:
-            return True
-        if solve_util(maze,x,y+1,sol)==True:
+        if (x,y)==(n-1,n-1) or go(x+1,y) or go(x,y+1):
             return True
         sol[x][y]=0
         return False
-    return False
-def print_solution(sol):
-    for i in sol:
-        for j in i:
-            print(j,end=" ")
-        print()
-maze=[[1,0,0,0],[1,1,0,1],[0,1,0,0],[1,1,1,1]]
-solve_maze(maze)    
+
+    if not go(0,0):
+        print("Solution doesn't exist")
+    else:
+        for r in sol:
+            print(' '.join(map(str,r)))
+
+if __name__=='__main__':
+    solve(maze)
