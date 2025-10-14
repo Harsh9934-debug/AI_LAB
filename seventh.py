dd034b6cda57ee031d94d56ade5610e5c3c6191e
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-14 23:31:02 +0530

    Updated the chanegs

--- seventh.py
+++ seventh.py
@@ -3,8 +3,8 @@ g={
     '2':['1','3','4'],
     '3':['1','2','4'],
     '4':['1','2','3']
-    }
+}
 c={}
 for v in sorted(g):
     c[v]=next(i for i in range(len(g)) if all (c.get(n)!=i for n in g[v]))
-print("Coloring of graph is",c)
+print("Coloring of graph is", c)
