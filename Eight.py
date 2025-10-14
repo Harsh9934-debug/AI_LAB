90e2021ca66de4be0aa3b85d2b461de0b5956e21
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 00:13:53 +0530

    Updated the changes

--- Eight.py
+++ Eight.py
@@ -1,12 +1,12 @@
 m=[[1,0,0,0],[1,1,0,1],[0,1,0,0],[1,1,1,1]]
 n=len(m)
-s=[[0]*n for _ in range(n)]
-def go(x,y):
- if x<0 or y<0 or x>=n or y>=n or m[x][y]==0 or s[x][y]:
-   return 0
- s[x][y]=1
- if (x,y)==(n-1,n-1)or go(x+1,y)or go(x,y+1):
-   return 1
- s[x][y]=0
-go(0,0)
+s=[[0]*n for _ in m]
+def f(x,y):
+    if not(0<=x<n and 0<=y<n and m[x][y]and not s[x][y]):
+        return 0
+    s[x][y]=1
+    if (x,y)==(n-1,n-1)or f(x+1,y)or f(x,y+1):
+        return 1
+    s[x][y]=0
+f(0,0)
 for r in s:print(*r)
