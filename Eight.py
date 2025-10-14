ee79317e3b4b0976cf3d380402d53845347e02d5
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 00:10:57 +0530

    Updated the changes

--- Eight.py
+++ Eight.py
@@ -1,25 +1,12 @@
-# Rat in a Maze problem using Backtracking
-
-maze = [[1,0,0,0],[1,1,0,1],[0,1,0,0],[1,1,1,1]]
-
-def solve(m):
-    n = len(m)
-    sol = [[0]*n for _ in range(n)]
-
-    def go(x,y):
-        if x<0 or y<0 or x>=n or y>=n or m[x][y]==0 or sol[x][y]:
-            return False
-        sol[x][y]=1
-        if (x,y)==(n-1,n-1) or go(x+1,y) or go(x,y+1):
-            return True
-        sol[x][y]=0
-        return False
-
-    if not go(0,0):
-        print("Solution doesn't exist")
-    else:
-        for r in sol:
-            print(' '.join(map(str,r)))
-
-if __name__=='__main__':
-    solve(maze)
+m=[[1,0,0,0],[1,1,0,1],[0,1,0,0],[1,1,1,1]]
+n=len(m)
+s=[[0]*n for _ in range(n)]
+def go(x,y):
+ if x<0 or y<0 or x>=n or y>=n or m[x][y]==0 or s[x][y]:
+   return 0
+ s[x][y]=1
+ if (x,y)==(n-1,n-1)or go(x+1,y)or go(x,y+1):
+   return 1
+ s[x][y]=0
+go(0,0)
+for r in s:print(*r)
