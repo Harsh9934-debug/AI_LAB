9deb1c262bb06d57e00c237bf3b49a573f8c3828
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 01:31:24 +0530

    Updated the changes

--- fifth.py
+++ fifth.py
@@ -1,9 +1,9 @@
 g=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 v,q=set(),[0]
 while q:
- n=q.pop(0)
- if n in v:continue
- print(n,end=' ')
- v.add(n)
- if n==3:break
- q+=g[n]
+    n=q.pop(0)
+    if n in v:continue
+    print(n,end=' ')
+    v.add(n)
+    if n==3:break
+    q+=g[n]
