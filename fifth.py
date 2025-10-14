d5e90ad20b01e24e0c72e83350395c5b92e481e8
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 00:18:53 +0530

    Updated the changes

--- fifth.py
+++ fifth.py
@@ -1,24 +1,9 @@
-# write a program to implement the Best First Search Algorithm
-
 from queue import PriorityQueue as PQ
-def s(g,S,G):
-    v,q=set(),PQ(); q.put((0,S))
-    while not q.empty():
-        c,n=q.get()
-        if n in v: continue
-        v.add(n)
-        print(n,end=" ")
-        if n==G: break
-        for nn,w in g[n].items():
-            if nn not in v: q.put((c+w,nn))
-
-graph={
-    'A':{'B':1,'C':4},
-    'B':{'A':1,'D':2,'E':5},
-    'C':{'A':4,'F':1},
-    'D':{'B':2},
-    'E':{'B':5,'F':3},
-    'F':{'C':1,'E':3}
-}
-print("Following is the Best First Search")
-s(graph,'A','F')  
+g=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+v,q=set(),PQ();q.put((0,0))
+while not q.empty():
+ _,n=q.get()
+ if n in v:continue
+ v.add(n);print(n,end=' ')
+ if n==3:break
+ [q.put((0,i)) for i in g[n] if i not in v]
