8bacabd0f062d1ca857e1b492a47d9242405547f
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-14 17:23:48 +0530

    Updated the program

--- seventh.py
+++ seventh.py
@@ -1,26 +1,18 @@
 # write a program to solve the graph coloring problem
 
-def color_vertices(graph):
-    vertices = sorted(list(graph.keys()))
-    color_graph = {}
-    for vertex in vertices:
-        unused_colours = [True] * len(vertices)
-        for neighbour in graph.get(vertex, []):
-            if neighbour in color_graph:
-                used = color_graph[neighbour]
-                if 0 <= used < len(unused_colours):
-                    unused_colours[used] = False
-        for color in range(len(vertices)):
-            if unused_colours[color]:
-                color_graph[vertex] = color
-                break
-    return color_graph
+def color_vertices(g):
+    vs = sorted(g)
+    c = {}
+    for v in vs:
+        used = {c[n] for n in g.get(v, []) if n in c}
+        c[v] = next(i for i in range(len(vs)) if i not in used)
+    return c
+
 if __name__ == '__main__':
-    graph = {
-        'A': ['B', 'C', 'D'],
-        'B': ['A', 'D'],
-        'C': ['A', 'D'],
-        'D': ['A', 'B', 'C', 'E'],
-        'E': ['D']
-    }
-    print("Coloring of graph is", color_vertices(graph))
+    g = {
+        'A':['B','C','D'],
+        'B':['A','D'],
+        'C':['A','D'],
+        'D':['A','B','C','E'],
+        'E':['D']}
+    print('Coloring of graph is', color_vertices(g))
