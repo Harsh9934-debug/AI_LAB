f05451a5f965e73de286e9db907ae50cb1fc3233
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-14 18:43:26 +0530

    Made this more shot

--- Sixth.py
+++ Sixth.py
@@ -2,6 +2,4 @@ from itertools import permutations
 
 g=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 print("Min cost:", 
-      min(sum(g[a][b] for a,b in zip((0,)+p,p+(0,))) 
-        for p in permutations([1,2,3]))
-        )
+      min(sum(g[a][b] for a,b in zip((0,)+p,p+(0,))) for p in permutations([1,2,3])))
