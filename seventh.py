9bcb0315659f6621d074a8ce3b87403315621cad
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-14 23:22:22 +0530

    Updated the changes

--- seventh.py
+++ seventh.py
@@ -1,8 +1,12 @@
-# Graph Coloring problem
+g={
+    '1': ['2','3','4'],
+    '2': ['1','3','4'],
+    '3': ['1','2','4'],
+    '4': ['1','2','3']
+}
 
-g=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-c=[-1]*len(g)
-for v in range(len(g)):
-    used={c[i]for i in range(len(g))if g[v][i]and c[i]!=-1}
-    c[v]=next(x for x in range(len(g))if x not in used)
-print("Represents the different Colors addigned to each vertices:",c)
+c={}
+for v in sorted(g):
+    c[v]=next(i for i in range(len(g)) 
+              if all(c.get(n)!=i for n in g[v]))
+print("Coloring of graph is", c)
