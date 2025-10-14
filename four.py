a8feef59ef116678b697e7d058344e2f681d76b8
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-14 11:48:54 +0530

    Updated the prg

--- four.py
+++ four.py
@@ -4,30 +4,30 @@ print("Water jug problem ")
 x=int(input("Enter X: "))
 y=int(input("Enter Y: "))
 while True:
-    rno=int(input("Enter the Rule number : "))
-    if rno==1:
+    sno=int(input("Enter the step number : "))
+    if sno==1:
         if x<4:
             x=4
-    if rno==2:  
+    if sno==2:  
         if y<3:
             y=3
-    if rno==5:
+    if sno==5:
         if x>0:
             x=0
-    if rno==6:
+    if sno==6:
         if y>0:
             y=0
-    if rno==7:
+    if sno==7:
         if x+y>=4 and y>0:
             x,y=4,y-(4-x)
-    if rno==8:
+    if sno==8:
         if x+y>=3 and x>0:
             x,y=x-(3-y),3
-    if rno==9:
+    if sno==9:
         if x+y<4 and y>0:
             x,y=x+y,0
 
-    if rno==10:
+    if sno==10:
         if x+y<3 and x>0:
             x,y=0,x+y
 
