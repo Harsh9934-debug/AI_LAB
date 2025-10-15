b802603cd4e143e10a3ac8aebbc45050f7779b83
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 18:51:17 +0530

    Uodated the changes

---
+++ python/3rd.py
@@ -0,0 +1,15 @@
+#write a program to perform string handler 
+
+college="Presidency"
+
+print("presidency has",len(college),"letters")
+print(college[0])
+print(college.upper())
+print(college.lower())
+print("college has",college.count('e'),"e")
+for i in college :
+    print(i)
+print(college[:3])
+print(college[3:])
+
+print(college.replace("Presidency","PES"))
