158b53ee37392e3ed4f4a1b3009fbfb2dfd1d421
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 22:14:27 +0530

    Updated the program

---
+++ python/15th.py
@@ -0,0 +1,11 @@
+# writa a program to demonstrate the math built in fumction 
+
+import math
+
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+print("The sum of two number is",math.fsum((x,y)))
+print("The factorial of x is",math.factorial(x))
+print("The square root of x is",math.sqrt(x))
+print("The power of x to y is",math.pow(x,y))
+print("The gcd of the two number is",math.gcd(x,y))
