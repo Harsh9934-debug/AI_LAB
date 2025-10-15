99e33e64f9b66956d287ca2eb2d1865430ae4e19
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 22:19:53 +0530

    Updated the changes

--- python/16th.py
+++
@@ -1,6 +0,0 @@
-# write a python program that asks the user for the input and print that number in the reversr order using the while loop 
-n=int(input("Enter the number "))
-
-while n>0:
-    print(n)
-    n-=1
