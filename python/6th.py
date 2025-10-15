d67ce6597591af96f07351a53fdb6976715fef03
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 18:57:55 +0530

    Updated the changes

---
+++ python/6th.py
@@ -0,0 +1,10 @@
+#write a program to demonstrate the operation on tuple
+
+a=('F','A','C','E')
+print(a[0])
+print(a[1:3])
+print(a[-1])
+print(a*2)
+print(a+('D','E','F'))
+print(a.index('C'))
+print(a.count('E')) 
