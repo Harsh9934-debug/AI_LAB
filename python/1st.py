cc135f706f4a0cadaf10f136ab4cb1da75dc4d3b
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-10-15 12:10:03 +0530

    Updated the program

---
+++ python/1st.py
@@ -0,0 +1,24 @@
+# write a program to demonstrate the datatypes in the pthon 
+
+a=10
+print(type(a))
+
+b=10.5
+print(type(b))
+
+c=1+2j
+print(type(c))
+
+d="hello"
+print(type(d))
+
+e=[1,2,3,4]
+print(type(e))
+
+
+f=(1,2,3,4)
+print(type(f))
+
+g={1:'a',2:'b',3:'c'}
+print(type(g))
+
